5ce836033bec49940b10a1adeb39c85871fb900b
Tom Elam <tomelam@gmail.com>
2010-06-30 10:48:11 +0530

    Added dojoWidget.html (copied from dijit/tests/layout/AccordionContainer.html and renamed). Deleted dojoWidget.js because its tests should be moved to dojoWidget.html.

--- tests/module.js
+++ tests/module.js
@@ -1,16 +1,12 @@
 dojo.provide("zen.tests.module");
 
-try {
-    //dojo.require("zen.tests.object");
-    //dojo.require("zen.tests.rulesDB");
+try{
+    dojo.require("zen.tests.object");
+    dojo.require("zen.tests.rulesDB");
     dojo.require("zen.tests.domNode");
-    dojo.require("zen.tests.dojoWidget");
-    //dojo.require("zen.tests.tree");
-    dojo.registerUrl("zen.tests.AccordionContainer",
-		     dojo.moduleUrl("zen", "tests/AccordionContainer.html"),
-		     99999999);
-    dojo.debug("Finishing load of module.js");
-								     
-} catch(e) {
-    console.debug("Exception: " + e);
-};
+    //dojo.require("zen.tests.dojoWidget");
+    doh.registerUrl("zen.tests.dojoWidget", dojo.moduleUrl("zen", "tests/dojoWidget.html"), 99999999);
+    dojo.require("zen.tests.tree");
+}catch(e){
+    doh.debug(e);
+}
